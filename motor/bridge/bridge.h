7563eb7988a076de3ec5ee147543e177038a1fc0
Tilt <tilt.silvie@gmail.com>
2013-03-05 21:54:09 +0900

    bridge.* を初期化（いらない行削除）

--- motor/bridge/bridge.h
+++ motor/bridge/bridge.h
@@ -3,8 +3,8 @@
 /*Author	: Makito Ishikura*/
 /**************************************/
 
-#ifndef	_HALL_H_
-#define	_HALL_H_
+#ifndef	_BRIDGE_H_
+#define	_BRIDGE_H_
 
 
 /**************************************/
@@ -13,41 +13,13 @@
 
 /**************************************/
 
-/**************************************/
-#define	NUM_PHASE	6
-/**************************************/
-
-
-
-/**************************************/
-#define	PHASE_1	0
-#define	PHASE_2	1
-#define	PHASE_3	2
-#define	PHASE_4	3
-#define	PHASE_5	4
-#define	PHASE_6	5
-/**************************************/
-
-/**************************************/
-void	initializeHall( void );
-
-unsigned char	getPhase( unsigned char hall_1, unsigned char hall_2, unsigned char hall_3 );
-
-unsigned char	getFowardPhase( unsigned char phase );
-unsigned char	getBackwardPhase( unsigned char phase );
-
-/**************************************/
 
 
 
 /**************************************/
 #ifdef	DO_TEST
 
-void	Test_getPhase( void );
-void	Test_getFowardPhase( void );
-void	Test_getBackwardPhase( void );
-
 #endif
 /**************************************/
 
-#endif	/*_HALL_H_*/
+#endif	/*_BRIDGE_H_*/
