ae9f71490f6020a0629e12f4851cefa29f5c4e9a
Tilt <tilt.silvie@gmail.com>
2013-04-04 20:32:05 +0900

    プロトタイプ宣言だけ記述

--- servo/servo.h
+++ servo/servo.h
@@ -5,3 +5,12 @@
  * Created on 2013/04/04 20:27
  */
 
+#ifndef	_SERVO_H_
+#define	_SERVO_H_
+
+/*******************************************/
+extern void	initializeServo( void );
+extern signed int	setReferenceServo( unsigned int ref );
+/*******************************************/
+
+#endif	/*_SERVO_H_*/
